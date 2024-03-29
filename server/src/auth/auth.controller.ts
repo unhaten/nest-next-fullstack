050ba9edd44faabd16ee01ca55c24e19e3ef2411
Max <csgold2002@mail.ru>
2024-03-29 19:10:47 +0300

    why so hard bruh

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,28 +1,33 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateUserDto } from 'src/user/dto/user.dto';
-import { UserService } from 'src/user/user.service';
-import { LoginDto } from './dto/auth.dto';
-import { AuthService } from './auth.service';
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { CreateUserDto } from 'src/user/dto/user.dto'
+import { UserService } from 'src/user/user.service'
+import { LoginDto } from './dto/auth.dto'
+import { AuthService } from './auth.service'
 
 @Controller('auth')
 export class AuthController {
-    constructor(private userService: UserService,
-        private authService: AuthService
-    ) { }
+	constructor(
+		private userService: UserService,
+		private authService: AuthService
+	) {}
 
-    @Post('register')
-    async registerUser(@Body() dto: CreateUserDto) {
-        return await this.userService.create(dto)
+	@Post('register')
+	async registerUser(@Body() dto: CreateUserDto) {
+		return await this.userService.create(dto)
+	}
+
+	@Get()
+	getAuthHello(): string {
+		return 'hello from auth'
+	}
+
+	@Post('login')
+	async login(@Body() dto: LoginDto) {
+		return await this.authService.login(dto)
+	}
+
+	@Post('refresh')
+	async refreshToken() {
+        
     }
-
-    @Get()
-    getAuthHello(): string {
-        return 'hello from auth'
-    }
-
-    @Post('login')
-    async login(@Body() dto: LoginDto) {
-        return await this.authService.login(dto)
-    }
-
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -29,7 +29,7 @@ export class AuthService {
                 })
             },
         }
-
+ 
     }
 
     async validateUser(dto: LoginDto) {
